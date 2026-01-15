6550f7c1b8f4e440477cd13b2f1d9c0e69e1410c
get <worawutavc@gmail.com>
2026-01-15 11:37:58 +0700

    90% : translation

--- src/_workspace/pages/leave-approval/page.tsx
+++ src/_workspace/pages/leave-approval/page.tsx
@@ -34,10 +34,10 @@ const InnerApp = () => {
   }, [isLoadingReactHookForm])
   const breadcrumbs = [
     <Typography key='1' sx={{ color: 'var(--mui-palette-text-secondary) !important' }}>
-      {t('Menu.Home')}
+      Home
     </Typography>,
     <Typography key='2' sx={{ color: 'var(--mui-palette-text-secondary) !important' }}>
-      {t('Menu.Leave Approval')}
+      Leave Approval
     </Typography>
   ]
   return (
@@ -45,7 +45,7 @@ const InnerApp = () => {
       <FormProvider {...reactHookFormMethods}>
         {/* Header Section */}
         <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center', gap: 4 }}>
-          <Typography variant='h4'>{t('Menu.Leave Approval')}</Typography>
+          <Typography variant='h4'>Leave Approval</Typography>
           <Divider orientation='vertical' flexItem />
           <Breadcrumbs separator='›' aria-label='breadcrumb' sx={{ display: 'inline-block' }}>
             {breadcrumbs}
